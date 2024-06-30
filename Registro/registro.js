48116d5b1f114b364d88cd879cd1b03a552f246b
Paau93 <141678206+Paau93@users.noreply.github.com>
2024-06-29 21:07:31 -0300

    Initial commit

---
+++ Registro/registro.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            type: 0, //login=0 - registro=1
+            form: {
+                email: "",
+                password: ""
+            }
+        }
+    },
+    methods: {
+        registro() {
+            console.log(this.form);
+        }
+
+    },
+    computed: {
+        titulo() {
+            return (this.type==0)?'Iniciar Sesión':'Registrarse';
+        }
+    }
+}).mount('#app')
